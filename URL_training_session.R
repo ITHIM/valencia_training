35bbf19907724dc16153f997ac572da1d3f913a4
usr110 <m.aliabbas@gmail.com>
2022-11-01 08:46:54 +0100

    Add URL for presentations

--- URL_training_session.R
+++ URL_training_session.R
@@ -1,1 +1,8 @@
+# Landing page and schedule for the day
 browseURL("https://ithim.github.io/valencia_training/training_materials/index.html")
+
+# Overview presentation
+browseURL("https://ithim.github.io/valencia_training/presentations/overview.html")
+
+# Implementation presentation
+browseURL("https://ithim.github.io/valencia_training/presentations/implementation.html#/title-slide")
